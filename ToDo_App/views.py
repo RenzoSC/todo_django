37b3b790be5af614dd6d589e4438e94f8bc7bfee
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2023-05-28 03:15:02 -0300

    some

--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -5,13 +5,17 @@ from .forms import createNewTask
 from datetime import date
 
 def home(request):
-    if request.method == 'GET':
-        form = createNewTask()
-        return render(request, 'index.html', {
-        'form': createNewTask(),})
+    form = createNewTask()
+    tasks = None
+    if request.method == 'GET' and 'f_date' in request.GET:
+        f_date = request.GET['f_date']
+        tasks = Task.objects.filter(f_date=f_date)
     elif(request.method == 'POST'):
-        
         f_date = request.POST['f_date']
         dia = Date.objects.create(date=f_date)
         Task.objects.create(title=request.POST['title'], description=request.POST['description'], f_date=dia)
         return redirect(home);
+    print(tasks)
+    return render(request, 'index.html', {
+        'form': form,
+        'tasks': tasks})
